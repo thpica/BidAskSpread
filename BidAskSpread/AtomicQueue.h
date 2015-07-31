1306cfaed6ace2f828283ce078187c29600483a8
Thomas Picariello <thpica@gmail.com>
2015-07-31 17:05:37 +0200

    AtomicQueue various optimisation
    empty is thread safe
    peek waits or throws
    default size 25K

--- BidAskSpread/AtomicQueue.h
+++ BidAskSpread/AtomicQueue.h
@@ -12,7 +12,7 @@ template<typename T>
 class AtomicQueue
 {
 public:
-	AtomicQueue(uint64_t queue_size = 5000): //5K queue max size
+	AtomicQueue(uint64_t queue_size = 25000): //25K queue max size
 		m_atEnd(false),
 		m_full(false),
 		m_queue(queue_size){}
@@ -39,34 +39,47 @@ public:
 	
 	void setQueueEnd(){ m_atEnd.store(true); }
 
-	bool empty(){ return m_queue.peek() == nullptr;	}
+	bool empty(){ return m_empty.load(); }
 
 	bool full(){ return m_full.load(); }
 
 	bool end(){ return m_atEnd.load() && empty(); }
 
-	T* peek(){ return m_queue.peek(); }
+	T* peek(){
+		T* ptr = nullptr;
+		while((ptr = m_queue.peek()) == nullptr){
+			if(end())
+				throw QueueEndException();
+			m_empty = true;
+			this_thread::yield();
+		}
+		m_empty = false;
+		return ptr;
+	}
 
 	T dequeue(){
 		T elem;
 		while(!m_queue.try_dequeue(elem)){
 			if(end())
 				throw QueueEndException();
+			m_empty = true;
 			this_thread::yield();
 		}
+		m_empty = false;
 		return elem;
 	}
 
 	void enqueue(T&& elem){
 		while(!m_queue.try_enqueue(elem)){
-			m_full.store(true);
+			m_full = true;
 			this_thread::yield();
 		}
-		m_full.store(false);
+		m_full = false;
 	}
 
 private:
 	atomic<bool> m_atEnd;
 	atomic<bool> m_full;
+	atomic<bool> m_empty;
 	ReaderWriterQueue<T> m_queue;
 };
